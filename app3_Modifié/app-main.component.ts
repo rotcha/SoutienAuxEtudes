a0c585bd0ee48686ad9c93a4b2c22136ea4f916c
Yves Israel <yves-israel.ngudie@polymtl.ca>
2017-01-03 22:20:15 -0500

    maj

---
+++ app3_Modifié/app-main.component.ts
@@ -0,0 +1,12 @@
+/**
+ * Created by louly on 2017-01-03.
+ */
+import {Component} from '@angular/core';
+import {PolyHackInfo} from './polyhackInfo.component';
+import {AboutComponent} from './about.component';
+
+@Component({
+    selector:'polyhack',
+    templateUrl:'app3_Modifié/app-main-template.html'
+})
+export class PolyHackApp{}
