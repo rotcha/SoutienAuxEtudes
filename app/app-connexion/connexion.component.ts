63eac7658c05d97cd3cfda53c66de401903b2351
Rodrigue <rodyfe2008@gmail.com>
2017-01-06 15:33:43 -0500

    ajout de app-connexion

---
+++ app/app-connexion/connexion.component.ts
@@ -0,0 +1,11 @@
+/**
+ * Created by Jenny on 2017-01-06.
+ */
+import {Component} from
+    '@angular/core';
+@Component({
+    moduleId: module.id,
+    selector:'connect',
+    templateUrl:'./app/app-connexion/connexion-template.html'
+})
+export class ConnexionComponent{};
